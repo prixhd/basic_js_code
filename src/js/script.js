3bde9d4a3c33bbaf9e280babf924dafced4ecfea
Manap <manap.aminov@gmail.com>
2022-08-07 20:58:33 +0300

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,3 @@
 "use strict";
 
-// document.write("Hello world")
-//console.log("hello wrld")
-//console.info('hello wrld')
-//console.error('heloo')
-//console.warn("hello wrld")
-
-let num = 4;
-
 
