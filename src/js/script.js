89346061e57748c4f941224732f4a1304d6f48e2
Manap <manap.aminov@gmail.com>
2022-08-07 15:26:35 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// document.write("Hello world")
+//console.log("hello wrld")
+//console.info('hello wrld')
+//console.error('heloo')
+//console.warn("hello wrld")
+
+let num = 4;
+
+
